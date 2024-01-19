c4eb6a2e8e68da731d24b309aa26421bf417cd40
tanayvaswani <vaswani.tanay9@gmail.com>
2024-01-19 20:21:01 +0530

    feat: why-us card

---
+++ components/why-us-section/why-us-card/why-us-card.tsx
@@ -0,0 +1,19 @@
+interface IWhyUsCard {
+    tagline: string;
+    headline: string;
+    description: string;
+}
+
+const WhyUsCard = ({
+    tagline,
+    headline,
+    description
+}: IWhyUsCard) => {
+    return ( 
+        <div>
+            any
+        </div>
+     );
+}
+ 
+export default WhyUsCard;
