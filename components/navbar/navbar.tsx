1105e4d279aa5299b7235367b3c04088886e8d31
tanayvaswani <vaswani.tanay9@gmail.com>
2024-01-19 16:02:54 +0530

    fix: layout

---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
+        
+    </nav>
+  );
+};
+
+export default Navbar;
